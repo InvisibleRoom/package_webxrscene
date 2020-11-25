3da3440c621e6dd860f2f0f21b1687c27e65f329
invisibleSascha <sascha@invisibleroom.com>
2020-11-25 22:51:00 +0100

    utils -> lerp, getpointincircle, getpointoncircle

--- src/Utils.js
+++ src/Utils.js
@@ -1,5 +1,22 @@
 const Utils = {
-
+  lerp : (v1, v2, alpha)=>{
+    alpha = alpha < 0 ? 0 : alpha;
+    alpha = alpha > 1 ? 1 : alpha;
+    return v1 + (v2 - v1) * alpha;
+  },
+  getPointInCircle : (point,target, radius) =>{
+    var distsq = (point.x - target.x) * (point.x - target.x) + (point.y - target.y) * (point.y - target.y) + (point.z - target.z) * (point.z - target.z);
+    // returns bool , distance to target origin 
+    return [distsq <= radius * radius * radius,distsq];
+  },
+  getPointOnCircle: (radius,deg, origin) =>{
+    var x = origin.x + Math.sin(deg * Math.PI / 180) * radius;//radius;// (Math.sin(deg * Math.PI / 180) * radius);
+    var y = origin.y + Math.cos(deg * Math.PI / 180) * radius;//radius; //+ (Math.cos(deg * Math.PI / 180) * radius);
+    return {
+        x : x,
+        y : y
+    }
+  },
   WorldToScreenPosition : (vector3,camera,container = null) =>{
     var size = {
       x : window.innerWidth,
