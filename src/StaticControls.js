fa8ee8cbaaecbe27f2dea980027d165fe280d547
invisibleSascha <sascha@invisibleroom.com>
2021-01-09 16:18:24 +0100

    staticcontrols init

--- src/StaticControls.js
+++ src/StaticControls.js
@@ -1,4 +1,5 @@
 import { Euler, Vector3 } from 'three';
+import { Utils } from './Utils';
 
 class StaticControls {
   minPolarAngle = 0;
@@ -52,6 +53,25 @@ class StaticControls {
 
     this.movement.x *= this.damping;
     this.movement.y *= this.damping;
+
+
+    
+    var targetScreenPos = Utils.WorldToScreenPosition(this.target, this.camera, this.domElement);
+    
+    if(
+      this.movement.x !== targetScreenPos.x ||
+      this.movement.y !== targetScreenPos.y
+      ){
+        var diffX = targetScreenPos.x - this.movement.x;
+        var diffY = targetScreenPos.y - this.movement.y;
+        
+        // this.movement.x += diffX * .1;
+        // this.movement.y += diffY * .1;
+        //console.log(targetScreenPos, this.movement, diffX , diffY);
+        //transition zu Target Position
+        //this.camera.lookAt(this.target);
+
+    }
   }
 
   onMouseDown = (event) =>{
