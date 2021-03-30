8e13ba9e391073f5552dbf88c793e551a4d66454
invisibleSascha <sascha@invisibleroom.com>
2021-03-30 14:19:22 +0200

    AddToScene (sceneName, model)

--- src/SceneController.js
+++ src/SceneController.js
@@ -3,46 +3,42 @@ import * as THREE from 'three';
 class SceneController{
   constructor(context){
     this.context = context;
+    this.activeScene = "default";
 
-    this.rooms = {
+    this.scenes = {
       default : new THREE.Scene()
     };
 
-    this.rooms.default.name = this.currentScene;
+    this.scenes.default.name = this.activeScene;
 
     //set default Scene
     this.SetActiveScene("default");
   }
 
-  AddToScene = (model) =>{
-    var roomName = "default";
-
-    if(model.userData.hasOwnProperty("Room")){
-      roomName = model.userData.Room;
+  AddToScene = (sceneName = "default", model) =>{
+    
+    if(!this.scenes.hasOwnProperty(sceneName)){
+      this.scenes[sceneName] = new THREE.Scene();
+      this.scenes[sceneName] = sceneName;
     }
 
-    if(!this.rooms.hasOwnProperty(roomName)){
-      this.rooms[roomName] = new THREE.Scene();
-      this.rooms[roomName] = roomName;
-    }
-
-    this.rooms[roomName].add(model);
+    this.scenes[sceneName].add(model);
   }
 
-  SetActiveScene = (name) => {
-    if(!this.rooms.hasOwnProperty(name)){
-      console.warn(`Scene with Name:"${name}" does not exist`);
+  SetActiveScene = (sceneName) => {
+    if(!this.scenes.hasOwnProperty(sceneName)){
+      console.warn(`Scene with Name:"${sceneName}" does not exist`);
       return false;
     }
 
 
-    if(this.currentScene === name){
-      console.warn(`Scene with Name:"${name}" is already current Scene`);
+    if(this.currentScene === sceneName){
+      console.warn(`Scene with Name:"${sceneName}" is already current Scene`);
       return false;
     }
 
-    this.context.Scene = this.rooms[name];
-    this.activeScene = this.rooms[name].name;
+    this.context.Scene = this.scenes[sceneName];
+    this.activeScene = this.scenes[sceneName].name;
   }
 }
 
