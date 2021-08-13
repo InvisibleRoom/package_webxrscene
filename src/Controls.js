53490f812a46a85dc25a2c61d0a0d10786b52043
Lea Lazer <lea.schorling@gmail.com>
2021-08-13 16:50:30 +0200

    clickdisabled on transition

--- src/Controls.js
+++ src/Controls.js
@@ -1,12 +1,12 @@
 import * as THREE from 'three';
-import {DesktopControls} from './DesktopControls';
-import {VRController} from './VRController';
+import { DesktopControls } from './DesktopControls';
+import { VRController } from './VRController';
 import { VRButton } from './VRButton.js';
 import { ARButton } from './ARButton.js';
 import { Vector3 } from 'three';
 
-class Controls{
-  constructor(context){
+class Controls {
+  constructor(context) {
 
     window.THREE = THREE;
     this.enabled = true;
@@ -23,9 +23,9 @@ class Controls{
     this.touchend = this.touchend.bind(this);
 
     this.size = this.getClientBox();
-    this.Desktop = new DesktopControls(context.Camera.instance,context.Renderer.instance.domElement, this.context),
-    
-    this.Update = this.Update.bind(this);
+    this.Desktop = new DesktopControls(context.Camera.instance, context.Renderer.instance.domElement, this.context),
+
+      this.Update = this.Update.bind(this);
     this.SetPosition = this.SetPosition.bind(this);
     this.SetTarget = this.SetTarget.bind(this);
     this.GetTarget = this.GetTarget.bind(this);
@@ -34,12 +34,13 @@ class Controls{
     this.cameraHelper = new THREE.Group();
     this.cameraHelper.name = "cameraHelper";
     this.context.Scene.add(this.cameraHelper);
+    this.isClickEnabled = true;
 
 
     //array of active elements in scene
     this.ActiveObjects = [];
     this.raycaster = new THREE.Raycaster();
-    
+
     this.SetupMouse();
     this.selectState = false;
 
@@ -51,26 +52,26 @@ class Controls{
     this.context.Events.registerEvent("OnChangeXRView");
 
     /** VR AR DOM ELEMENTS - Buttons */
-    this.vrButton = VRButton.createButton(this.context.Renderer.instance,this.context);
-    this.arButton = ARButton.createButton(this.context.Renderer.instance,this.context);
+    this.vrButton = VRButton.createButton(this.context.Renderer.instance, this.context);
+    this.arButton = ARButton.createButton(this.context.Renderer.instance, this.context);
 
     this.SetupVR = this.SetupVR.bind(this);
     this.SetupAR = this.SetupAR.bind(this);
 
-    this.context.Events.addEventListener("OnChangeXRView",(settings)=>{
+    this.context.Events.addEventListener("OnChangeXRView", (settings) => {
 
       this.currentControls = settings.xrMode;
 
-      switch(settings.xrMode){
+      switch (settings.xrMode) {
         case "VR":
           this.SetupVR(settings);
-        break;
+          break;
         case "AR":
           this.SetupAR(settings);
-        break;
+          break;
         default:
           this.SetupDesktop(settings);
-        break;
+          break;
       }
     });
 
@@ -78,108 +79,112 @@ class Controls{
     this.GetVRButton = this.GetVRButton.bind(this);
 
     /**VR Controls */
-    this.vr_controller = new VRController( this.context );
+    this.vr_controller = new VRController(this.context);
 
-    this.context.Scene.add( this.vr_controller.controllerGrips[ 0 ], this.vr_controller.controllers[ 0 ] );
+    this.context.Scene.add(this.vr_controller.controllerGrips[0], this.vr_controller.controllers[0]);
 
-    this.vr_controller.controllers[ 0 ].addEventListener( 'selectstart', ()=> { 
+    this.vr_controller.controllers[0].addEventListener('selectstart', () => {
       this.selectState = true;
-      
-      this.context.Events.dispatchEvent("mouse-down",{});
-    
+
+      this.context.Events.dispatchEvent("mouse-down", {});
+
     });
-    this.vr_controller.controllers[ 0 ].addEventListener( 'selectend', ()=> { 
+    this.vr_controller.controllers[0].addEventListener('selectend', () => {
       this.selectState = false;
-      this.context.Events.dispatchEvent("mouse-up",{});
+      this.context.Events.dispatchEvent("mouse-up", {});
     });
 
-    this.context.Events.addEventListener("OnAnimationLoop", this.Update );
+    this.context.Events.addEventListener("OnAnimationLoop", this.Update);
 
   }
 
-  SetupMouse(settings){
+  SetClickDisabled = (clickDisabled) => {
+    this.clickDisabled = clickDisabled
+  }
+
+  SetupMouse(settings) {
     this.mouse = new THREE.Vector2();
     this.mouse.x = null;
     this.mouse.y = null;
 
     this.selectState = false;
 
-    this.context.Renderer.instance.domElement.addEventListener( 'pointermove',this.mousemove);
-    this.context.Renderer.instance.domElement.addEventListener( 'pointerdown', this.mousedown);
-    this.context.Renderer.instance.domElement.addEventListener( 'pointerup', this.mouseup);
-    this.context.Renderer.instance.domElement.addEventListener( 'touchstart', this.touchstart);
-    this.context.Renderer.instance.domElement.addEventListener( 'touchend', this.touchend);
+    this.context.Renderer.instance.domElement.addEventListener('pointermove', this.mousemove);
+    this.context.Renderer.instance.domElement.addEventListener('pointerdown', this.mousedown);
+    this.context.Renderer.instance.domElement.addEventListener('pointerup', this.mouseup);
+    this.context.Renderer.instance.domElement.addEventListener('touchstart', this.touchstart);
+    this.context.Renderer.instance.domElement.addEventListener('touchend', this.touchend);
   }
-  getClientBox(){
+  getClientBox() {
 
     var size = {
-      width : this.context.Renderer.instance.domElement.width,
-      height : this.context.Renderer.instance.domElement.height,
-      x : 0,
-      y : 0
+      width: this.context.Renderer.instance.domElement.width,
+      height: this.context.Renderer.instance.domElement.height,
+      x: 0,
+      y: 0
     };
     return size;
   }
-  GetCurrentXRMode(){
+  GetCurrentXRMode() {
     return this.currentControls;
   }
   mousedown = () => { this.selectState = true; }
   mouseup = () => { this.selectState = false; }
-  mousemove(e){
-    
-    if(this.size.width === 0 || this.size.height === 0){
+  mousemove(e) {
+
+    if (this.size.width === 0 || this.size.height === 0) {
       this.getClientBox();
     }
 
-    this.mouse.x = ( e.clientX / this.size.width  ) * 2 - 1;
-    this.mouse.y = - ( e.clientY / this.size.height ) * 2 + 1;
+    this.mouse.x = (e.clientX / this.size.width) * 2 - 1;
+    this.mouse.y = - (e.clientY / this.size.height) * 2 + 1;
   }
-  touchstart(e){
+  touchstart(e) {
     this.selectState = true;
-    this.mouse.x = ( e.touches[0].clientX / this.size.width ) * 2 - 1;
-    this.mouse.y = - ( e.touches[0].clientY / this.size.height ) * 2 + 1;
+    this.mouse.x = (e.touches[0].clientX / this.size.width) * 2 - 1;
+    this.mouse.y = - (e.touches[0].clientY / this.size.height) * 2 + 1;
   }
-  touchend(e){
-      this.selectState = false;
-      this.mouse.x = null;
-      this.mouse.y = null;
+  touchend(e) {
+    this.selectState = false;
+    this.mouse.x = null;
+    this.mouse.y = null;
   }
-//
-  ChangeToDefault = ()=>{
-    if(this.currentControls == "Desktop"){
+  //
+  ChangeToDefault = () => {
+    if (this.currentControls == "Desktop") {
       this.Desktop.ChangeToDefault();
 
       this.getClientBox();
     }
   }
-  ChangeToStatic = ()=>{
+  ChangeToStatic = () => {
 
     console.log("%c Change TO Static ", "background:#eee;");
 
-    if(this.currentControls == "Desktop"){
+    if (this.currentControls == "Desktop") {
       this.Desktop.ChangeToStatic();
       this.getClientBox();
     }
   }
-  ChangeScene = (sceneName)=>{
+  ChangeScene = (sceneName) => {
 
     console.log(`%c Change Scene => Controls: ${sceneName}`, "background:red;color:#fff");
     this.context.Scene.attach(this.cameraHelper);
-    
+
   }
   SetActiveCamera = (camera, sceneName) => {
 
     this.Desktop.SetActiveCamera(camera);
 
-    
+
 
     this.getClientBox();
 
   }
 
-  SetupDesktop(settings){
-    
-    if(this.context.Camera.instance.parent.name == "cameraHelper"){
+  SetupDesktop(settings) {
+
+    if (this.context.Camera.instance.parent.name == "cameraHelper") {
       var pos = this.context.Camera.instance.parent.position.clone();
       var rot = this.context.Camera.instance.parent.rotation.clone();
 
@@ -189,16 +194,16 @@ class Controls{
       this.context.Camera.instance.rotation = rot;
       this[this.currentControls].instance.update();
     }
-    this.context.Renderer.instance.setClearColor(0xffffff,0);
+    this.context.Renderer.instance.setClearColor(0xffffff, 0);
     this.getClientBox();
   }
 
-  SetupVR(settings){
-    
+  SetupVR(settings) {
+
     var vrCamera = this.context.Renderer.instance.xr.getCamera(this.context.Camera.instance);
     //this.cameraHelper.add(vrCamera);
     var _position = vrCamera.position.clone();
-    this.cameraHelper.position.set(_position.x,_position.y,_position.z);
+    this.cameraHelper.position.set(_position.x, _position.y, _position.z);
     this.cameraHelper.attach(this.context.Camera.instance);
 
     this.Desktop.SetEnabled(false);
@@ -206,132 +211,132 @@ class Controls{
     //this.cameraHelper.position.set(_position.x,_position.y,_position.z);
 
     this.context.Renderer.instance.autoClear = true;
-    this.context.Renderer.instance.setClearColor(0xffffff,1);
-  
-    this.vr_controller.controllerGrips.forEach((controller)=>{
+    this.context.Renderer.instance.setClearColor(0xffffff, 1);
+
+    this.vr_controller.controllerGrips.forEach((controller) => {
       controller.userData.noClip = true;
       this.cameraHelper.add(controller);
     });
-    this.vr_controller.controllers.forEach((controller)=>{
+    this.vr_controller.controllers.forEach((controller) => {
       controller.userData.noClip = true;
       this.cameraHelper.add(controller);
     });
 
-    
+
 
     this.getClientBox();
   }
 
-  SetupAR(){
+  SetupAR() {
 
     this.cameraHelper.add(this.context.Camera.instance);
     var _position = this.context.Camera.instance.position.clone();
     // this.context.Camera.instance.position.set(0,0,0);
     // this.cameraHelper.position.set(_position.x,_position.y,_position.z);
-    this.context.Renderer.instance.setClearColor(0xffffff,0);
+    this.context.Renderer.instance.setClearColor(0xffffff, 0);
 
     // this.vr_controller.controllerGrips.forEach((controller)=>{
-// 
-      // this.cameraHelper.attach(controller)
+    // 
+    // this.cameraHelper.attach(controller)
     // });
     // this.vr_controller.controllers.forEach((controller)=>{
-      // this.cameraHelper.attach(controller);
+    // this.cameraHelper.attach(controller);
     // });
 
     this.getClientBox();
   }
 
-  GetVRButton(){
+  GetVRButton() {
     return this.vrButton;
   }
-  
-  GetARButton(){
+
+  GetARButton() {
     return this.arButton;
   }
-  Update(t){
+  Update(t) {
 
-    if(this.ActiveObjects.length > 0 && this.interactivityEnabled){
+    if (this.ActiveObjects.length > 0 && this.interactivityEnabled && !this.clickDisabled) {
       this.FindIntersection();
     }
     // if(this.currentControls == "VR"){
     //   this.vr_controller.Update();      
     // }
 
-    if(this.currentControls == "Desktop"){
+    if (this.currentControls == "Desktop") {
       this[this.currentControls].instance.enabled = this.enabled;
       this[this.currentControls].instance.update(t);
     }
   }
-  GetPosition(){
+  GetPosition() {
     return this.context.Camera.instance.position;
   }
 
-  SetPosition (x,y,z){
+  SetPosition(x, y, z) {
 
     switch (this.currentControls) {
       case "Desktop":
-        this.context.Camera.instance.position.set(x,y,z);
-      break;
+        this.context.Camera.instance.position.set(x, y, z);
+        break;
       case "VR":
-        this.cameraHelper.position.set(x,y,z);
-      break;
-    
+        this.cameraHelper.position.set(x, y, z);
+        break;
+
       default:
-      break;
+        break;
     }
   }
-  SetTarget (x,y,z){
-    
-     switch (this.currentControls) {
+  SetTarget(x, y, z) {
+
+    switch (this.currentControls) {
       case "VR":
-        this.cameraHelper.lookAt(new THREE.Vector3(x,y,z));
+        this.cameraHelper.lookAt(new THREE.Vector3(x, y, z));
         this.cameraHelper.rotation.x = 0;
         this.cameraHelper.rotation.z = 0;
-      break;
+        break;
       default:
-        this[this.currentControls].SetTarget(x,y,z);
-      break;
+        this[this.currentControls].SetTarget(x, y, z);
+        break;
     }
   }
 
-  GetTarget(){
-   
-    switch(this.currentControls){
+  GetTarget() {
+
+    switch (this.currentControls) {
       case "VR":
-        return new Vector3(0,0,0);//{x:0,y:0,z:0}
-      break;
+        return new Vector3(0, 0, 0);//{x:0,y:0,z:0}
+        break;
       default:
         return this[this.currentControls].instance.target;
-      break;
+        break;
     }
   }
 
-  GetCameraPosition(){
+  GetCameraPosition() {
 
-    
-    switch(this.currentControls){
+
+    switch (this.currentControls) {
       case "VR":
-        
+
         return this.context.Renderer.instance.xr.getCamera(this.context.Camera.instance).position;
-      break;
+        break;
       case "AR":
         return this.context.Renderer.instance.xr.getCamera(this.context.Camera.instance).position;
-       // return this.context.Camera.instance.position;//this.cameraHelper.position;
-      break;
+        // return this.context.Camera.instance.position;//this.cameraHelper.position;
+        break;
       default:
         return this.context.Camera.instance.position;
-      break;
+        break;
     }
   }
 
-  GetInputPosition(){
-    if(this.currentControls == "VR"){
+  GetInputPosition() {
+    if (this.currentControls == "VR") {
       console.log(this.vr_controller);
       return this.vr_controller.controllers[0].position;
     }
 
-    if(this.currentControls == "Desktop"){
-      if ( this.mouse.x !== null && this.mouse.y !== null ) {
+    if (this.currentControls == "Desktop") {
+      if (this.mouse.x !== null && this.mouse.y !== null) {
         console.log(this.mouse);
         return this.mouse;
       }
@@ -342,16 +347,16 @@ class Controls{
 
   Raycast() {
 
-    return this.ActiveObjects.reduce( (closestIntersection, obj)=> {
+    return this.ActiveObjects.reduce((closestIntersection, obj) => {
       if (!obj.isClickEnabled) {
         return closestIntersection
       }
 
-      const intersection = this.raycaster.intersectObject( obj, true );
+      const intersection = this.raycaster.intersectObject(obj, true);
 
-      if ( !intersection[0] ) return closestIntersection;
+      if (!intersection[0]) return closestIntersection;
 
-      if ( !closestIntersection || intersection[0].distance < closestIntersection.distance ) {
+      if (!closestIntersection || intersection[0].distance < closestIntersection.distance) {
 
         intersection[0].object = obj;
 
@@ -363,54 +368,54 @@ class Controls{
 
       };
 
-    }, null );
+    }, null);
 
   }
-  FindIntersection(){
+  FindIntersection() {
     // Find closest intersecting object
     let intersect;
-    if(this.currentControls == "VR"){
-      this.vr_controller.SetFromController( 0, this.raycaster.ray );
- 
+    if (this.currentControls == "VR") {
+      this.vr_controller.SetFromController(0, this.raycaster.ray);
+
       intersect = this.Raycast();
       //Position the little white dot at the end of the controller pointing ray
-      if ( intersect ) this.vr_controller.SetPointerAt( 0, intersect.point );
+      if (intersect) this.vr_controller.SetPointerAt(0, intersect.point);
 
     }
 
-    if(this.currentControls == "Desktop"|| this.currentControls == "VR"){
-      if ( this.mouse.x !== null && this.mouse.y !== null ) {
-        
-        this.raycaster.setFromCamera(this.mouse, this.context.Camera.instance );
+    if (this.currentControls == "Desktop" || this.currentControls == "VR") {
+      if (this.mouse.x !== null && this.mouse.y !== null) {
+
+        this.raycaster.setFromCamera(this.mouse, this.context.Camera.instance);
         intersect = this.Raycast();
       }
     }
 
     //TODO: Not only for UI elements
-    if ( intersect && intersect.object.isClickEnabled ) {
+    if (intersect && intersect.object.isClickEnabled) {
 
-      if ( this.selectState ) {
+      if (this.selectState) {
         // Component.setState internally call component.set with the options you defined in component.setupState
-        intersect.object.setState( 'selected' );
-        this.context.Events.dispatchEvent("ui-mouse-down", intersect.object); 
+        intersect.object.setState('selected');
+        this.context.Events.dispatchEvent("ui-mouse-down", intersect.object);
       } else {
         // Component.setState internally call component.set with the options you defined in component.setupState
-        intersect.object.setState( 'hovered' );
-        this.context.Events.dispatchEvent("ui-hovered", intersect.object); 
+        intersect.object.setState('hovered');
+        this.context.Events.dispatchEvent("ui-hovered", intersect.object);
       };
     }
 
     //Deselect every activeObject that is not the current intersect object
-      this.ActiveObjects.forEach( (obj)=> {
+    this.ActiveObjects.forEach((obj) => {
 
-        if ( (!intersect || obj !== intersect.object) && (obj.isUI || obj.isClickEnabled) ) {
-          // Component.setState internally call component.set with the options you defined in component.setupState
-          obj.setState( 'idle' , obj);
-          this.context.Events.dispatchEvent("ui-idle", null); 
-        };
-      });
+      if ((!intersect || obj !== intersect.object) && (obj.isUI || obj.isClickEnabled)) {
+        // Component.setState internally call component.set with the options you defined in component.setupState
+        obj.setState('idle', obj);
+        this.context.Events.dispatchEvent("ui-idle", null);
+      };
+    });
   }
 
 }
 
-export {Controls};
+export { Controls };
