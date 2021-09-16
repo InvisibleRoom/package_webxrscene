533cf9f5a3c85c7d6c9fe0fbc4b0cb07f73d119a
invisibleSascha <sascha@invisibleroom.com>
2021-09-16 08:29:56 +0200

    house keeping

--- src/DesktopControls.js
+++
@@ -1,9 +0,0 @@
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-
-class DesktopControls {
-  constructor(camera,domElement){
-    this.instance = new OrbitControls(camera,domElement);
-  }
-}
-
-export {DesktopControls};
