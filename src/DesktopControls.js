c47b39791ea88dab379ff1438ade490a34886357
invisibleSascha <sascha@invisibleroom.com>
2022-11-01 14:10:44 +0100

    add Map Controls

--- src/DesktopControls.js
+++ src/DesktopControls.js
@@ -1,55 +1,70 @@
+import { Vector3 } from "three/build/three.module";
+import {
+	MapControls,
+	OrbitControls,
+} from "three/examples/jsm/controls/OrbitControls";
 
-import { Vector3 } from 'three/build/three.module';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-import {StaticControls} from './StaticControls';
+import { MOUSE, TOUCH } from "three";
+
+import { StaticControls } from "./StaticControls";
 
 class DesktopControls {
-  
-  constructor(camera,domElement, context = null){
-    this.context = context;
-    this.orbit = new OrbitControls(camera,domElement);
-    this.static = new StaticControls(camera,domElement, context);
-    this.static.enabled = false;
-    
-    this.instance = this.orbit;
-    // this.instance.enableZoom = true;
-		// this.instance.enablePan = false;
-		// this.instance.enableDamping = true;
-    // this.instance.rotateSpeed = 0.5;
-    
-  }
+	constructor(camera, domElement, context = null) {
+		this.context = context;
+		this.orbit = new OrbitControls(camera, domElement);
+		this.static = new StaticControls(camera, domElement, context);
+		this.static.enabled = false;
 
-  ChangeToStatic = () =>{
+		this.map = new MapControls(camera, domElement, context);
+		this.map.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+		this.map.dampingFactor = 0.05;
+		this.map.screenSpacePanning = false;
+		this.map.minDistance = 0.1;
+		this.map.maxDistance = 50;
+		this.map.maxPolarAngle = Math.PI / 2;
 
-    this.instance = this.static;
-    this.static.SetActive(true);
-    this.orbit.enabled = false;
-  }
-  ChangeToDefault = () =>{
-    this.instance = this.orbit;
-    this.static.SetActive(false);
-    this.orbit.enabled = true;
-  }
+		this.instance = this.orbit;
+	}
 
-  SetTarget = (x,y,z)=>{
-    this.static.target.set(x,y,z);
-    this.orbit.target.set(x,y,z);
+	ChangeToStatic = (changeToMapControls = false) => {
+		console.log("ChangeToStatic", changeToMapControls);
+		if (changeToMapControls) {
+			this.instance = this.map;
+			this.orbit.enabled = false;
+			this.static.SetActive(false);
+			return;
+		}
 
-    this.static.update();
-    this.orbit.update();
-  }
+		this.instance = this.static;
+		this.static.SetActive(true);
+		this.orbit.enabled = false;
+	};
+	ChangeToDefault = () => {
+		this.instance = this.orbit;
+		this.static.SetActive(false);
+		this.orbit.enabled = true;
 
-  SetActiveCamera = (camera) =>{
-    this.orbit.object = camera;
-    this.static.object = camera;
-  }
+		console.log("ChangeToDefault");
+	};
 
-  SetEnabled = (boolean)=>{
-    this.orbit.enabled = boolean;
-    this.static.enabled = boolean;
-    this.static.controls.enabled = boolean;
-  }
-  
+	SetTarget = (x, y, z) => {
+		this.static.target.set(x, y, z);
+		this.orbit.target.set(x, y, z);
+
+		this.static.update();
+		this.orbit.update();
+	};
+
+	SetActiveCamera = (camera) => {
+		this.orbit.object = camera;
+		this.static.object = camera;
+	};
+
+	SetEnabled = (boolean) => {
+		this.orbit.enabled = boolean;
+		this.static.enabled = boolean;
+		this.static.controls.enabled = boolean;
+	};
 }
 
-export {DesktopControls};
+export { DesktopControls };
